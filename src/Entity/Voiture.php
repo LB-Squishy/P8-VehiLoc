db7abc0704b59544963f0c8ff067dda8847e080a
LB-Squishy <laurentleboulbar@gmail.com>
2025-03-07 11:27:49 +0100

    Ajout de l'entité Voiture et des migrations associées

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $prixMensuel = null;
+
+    #[ORM\Column]
+    private ?float $prixJournalier = null;
+
+    #[ORM\Column]
+    private ?int $places = null;
+
+    #[ORM\Column]
+    private ?bool $transmission = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrixMensuel(): ?float
+    {
+        return $this->prixMensuel;
+    }
+
+    public function setPrixMensuel(float $prixMensuel): static
+    {
+        $this->prixMensuel = $prixMensuel;
+
+        return $this;
+    }
+
+    public function getPrixJournalier(): ?float
+    {
+        return $this->prixJournalier;
+    }
+
+    public function setPrixJournalier(float $prixJournalier): static
+    {
+        $this->prixJournalier = $prixJournalier;
+
+        return $this;
+    }
+
+    public function getPlaces(): ?int
+    {
+        return $this->places;
+    }
+
+    public function setPlaces(int $places): static
+    {
+        $this->places = $places;
+
+        return $this;
+    }
+
+    public function isTransmission(): ?bool
+    {
+        return $this->transmission;
+    }
+
+    public function setTransmission(bool $transmission): static
+    {
+        $this->transmission = $transmission;
+
+        return $this;
+    }
+}
