869a312d318521d0f6eaf8778be91a0fa63ec702
LB-Squishy <laurentleboulbar@gmail.com>
2025-03-07 14:41:30 +0100

    Ajout de la fonctionnalité de suppression de voiture avec redirection

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Repository\VoitureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class VoituresController extends AbstractController
 {
-    public function __construct(private VoitureRepository $voitureRepository) {}
+    public function __construct(
+        private VoitureRepository $voitureRepository,
+        private EntityManagerInterface $entityManagerInterface,
+    ) {}
 
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(): Response
@@ -22,7 +26,7 @@ final class VoituresController extends AbstractController
     }
 
     #[Route('/voiture/{id}', name: 'app_car_show', methods: ['GET'])]
-    public function voiture(int $id): Response
+    public function showVoiture(int $id): Response
     {
         $voiture = $this->voitureRepository->find($id);
 
@@ -33,5 +37,19 @@ final class VoituresController extends AbstractController
         return $this->render('voitures/show.html.twig', [
             'voiture' => $voiture,
         ]);
+    }
+
+    #[Route('/voiture/{id}/delete', name: 'app_car_delete', methods: ['GET'])]
+    public function deleteVoiture(int $id): Response
+    {
+        $voiture = $this->voitureRepository->find($id);
+
+        if (!$voiture) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $this->entityManagerInterface->remove($voiture);
+        $this->entityManagerInterface->flush();
+        return $this->redirectToRoute('app_home');
     }
 }
